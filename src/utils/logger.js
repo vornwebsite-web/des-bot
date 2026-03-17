b50a8ef2ac8c1b15cccc94533dd01296d0a2d4e0
vornwebsite-web <vornwebsite@gmail.com>
2026-03-17 15:40:17 +0200

    Create logger.js

---
+++ src/utils/logger.js
@@ -0,0 +1,18 @@
+const winston = require('winston');
+const path = require('path');
+const fs = require('fs');
+const logDir = path.join(__dirname, '../../logs');
+if (!fs.existsSync(logDir)) fs.mkdirSync(logDir, { recursive: true });
+
+module.exports = winston.createLogger({
+  level: 'info',
+  format: winston.format.combine(
+    winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+    winston.format.printf(({ timestamp, level, message }) => `[${timestamp}] [${level.toUpperCase()}] ${message}`)
+  ),
+  transports: [
+    new winston.transports.Console({ format: winston.format.combine(winston.format.colorize(), winston.format.simple()) }),
+    new winston.transports.File({ filename: path.join(logDir, 'error.log'), level: 'error' }),
+    new winston.transports.File({ filename: path.join(logDir, 'combined.log') }),
+  ],
+});
